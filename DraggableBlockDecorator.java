b04320f376dd71d0e58c19f499169e8557db227a
aleahgarcia <90589265+aleahgarcia@users.noreply.github.com>
2023-07-22 13:12:52 -0700

    add blocks to ConnectedBlock list
    will improve on the second sprint but kinda works now

--- DraggableBlockDecorator.java
+++ DraggableBlockDecorator.java
@@ -39,26 +39,45 @@ public class DraggableBlockDecorator extends Block {
     }
 
     private void snapToOtherBlock() {
+        boolean snapped = false;
+
         for (Component component : getParent().getComponents()) {
             if (component instanceof DraggableBlockDecorator && component != this) {
                 DraggableBlockDecorator otherBlock = (DraggableBlockDecorator) component;
 
                 int thisBottom = getY() + getHeight();
                 int otherBottom = otherBlock.getY() + otherBlock.getHeight();
-                int snapThreshold = 20; // Adjust for sensitivity
+                int snapThreshold = 10; // Adjust this value as needed for snapping sensitivity
 
+                // Check if the bottom edge of this block is close to the top edge of the other block
                 if (Math.abs(thisBottom - otherBlock.getY()) < snapThreshold) {
+                    // Snap this block to the top of the other block
                     setLocation(getX(), otherBlock.getY() - getHeight());
-                    return;
+                    snapped = true;
+                    break;
                 }
 
-
+                // Check if the top edge of this block is close to the bottom edge of the other block
                 if (Math.abs(getY() - otherBottom) < snapThreshold) {
+                    // Snap this block to the bottom of the other block
                     setLocation(getX(), otherBlock.getY() + otherBlock.getHeight());
-                    return;
+                    // Add this block to the connectedBlocks list
+                    DataSource.getInstance().addConnectedBlock(this);
+                    snapped = true;
+                    break;
                 }
             }
         }
+
+        // not snapped to any other block remove it from connectedBlocks list
+        if (!snapped) {
+            DataSource.getInstance().getConnectedBlocks().remove(this);
+        }
+
+        // System.out.println("Connected Blocks:");
+        // for (Block block : DataSource.getInstance().getConnectedBlocks()) {
+        //     System.out.println(block.getType());
+        // }
     }
 
 
