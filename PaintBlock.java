7a12e469bd53a9ce7abb6bd7083368ee4f9d986a
aleahgarcia <90589265+aleahgarcia@users.noreply.github.com>
2023-07-22 13:03:06 -0700

    Create PaintBlock.java

---
+++ PaintBlock.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PaintBlock extends Block {
+
+    Color PaintCustom = new Color(199, 125, 72);
+    public PaintBlock(String type) {
+        super(type);
+        setBackground(PaintCustom);
+    }
+
+    @Override
+    public void draw() {
+        JLabel textLabel = new JLabel(type);
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+
+        add(textLabel);
+    }
+}
