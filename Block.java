f7c6f453fdfa4ca3ec9a152cfcbd6569b94b9d54
aleahgarcia <90589265+aleahgarcia@users.noreply.github.com>
2023-07-21 20:31:41 -0700

    added getType and changed look of Blocks

--- Block.java
+++ Block.java
@@ -2,31 +2,37 @@ import javax.swing.*;
 import java.awt.*;
 
 public abstract class Block extends JPanel {
-    protected String label; // Text inside the block
+    protected String type;
 
-    public Block(String label) {
-        this.label = label;
-        setPreferredSize(new Dimension(70, 40));
+    public Block(String type) {
+        this.type = type;
+        setPreferredSize(new Dimension(30, 20));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
     }
 
     public abstract void draw();
 
+    public String getType(){
+        return this.type;
+
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        // Draw the block background
+        
         g.setColor(getBackground());
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        // Draw the label
+
         g.setColor(Color.BLACK);
-        g.setFont(new Font("Arial", Font.PLAIN, 18));
+        Font boldFont = new Font("Arial", Font.BOLD, 18);
+        g.setFont(boldFont);
         FontMetrics fontMetrics = g.getFontMetrics();
-        int labelWidth = fontMetrics.stringWidth(label);
+        int labelWidth = fontMetrics.stringWidth(type);
         int labelHeight = fontMetrics.getHeight();
         int labelX = (getWidth() - labelWidth) / 2;
         int labelY = (getHeight() + labelHeight) / 2 - fontMetrics.getDescent();
-        g.drawString(label, labelX, labelY);
+        g.drawString(type, labelX, labelY);
     }
 }
