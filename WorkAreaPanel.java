daaec4ae113aa9e1b749761765bae9285a0b0751
aleahgarcia <90589265+aleahgarcia@users.noreply.github.com>
2023-07-23 13:56:15 -0700

    Update WorkAreaPanel.java

--- WorkAreaPanel.java
+++ WorkAreaPanel.java
@@ -5,15 +5,27 @@ import java.awt.event.MouseListener;
 
 public class WorkAreaPanel extends JPanel implements MouseListener {
 
+    private int grayRectWidth;
+    private Block connectHere;
+
     public WorkAreaPanel() {
-        setBackground(Color.lightGray);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        grayRectWidth = (int) (screenWidth * 0.2);
+        setBackground(Color.WHITE);
+        connectHere = new DraggableBlockDecorator(new MoveBlock("Connect Here", 150, 25), 150, 25);
+        connectHere.setBounds(50, 100, 100, 25);
+        this.add(connectHere);
     }
 
-
     @Override
-    public void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        // Draw the gray rectangle on the right side
+        g.setColor(Color.GRAY);
+        g.fillRect(getWidth() - grayRectWidth, 0, grayRectWidth, getHeight());
     }
 
     @Override
