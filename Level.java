58d6a141619006d451583df84a4697c22d0b3817
rishabb02 <67344869+rishabb02@users.noreply.github.com>
2023-07-22 20:32:02 -0700

    level one now has row and col input

--- Level.java
+++ Level.java
@@ -1,7 +1,7 @@
 import java.util.random.*;
 public class Level {
     public int level;
-    private static Cell[][] cells;
+    public static Cell[][] cells;
 
     public Level(int level) {
         this.level = level;
@@ -13,20 +13,35 @@ public class Level {
     }
 
     public static Cell[][] getLevel(int level){
+//        switch(level){
+//            case 1:
+//                cells = new Cell[2][2];
+//                cells[0][0] = new Cell(1, Cell.Diamond.DEFAULT, true);
+//                cells[0][1] = new Cell(2, Cell.Diamond.RED, false);
+//                cells[1][0] = new Cell(3, Cell.Diamond.GREEN, false);
+//                cells[1][1] = new Cell(4, Cell.Diamond.GREEN, false);
+//                break;
+//            case 2:
+//                cells = new Cell[3][3];
+//                cells[0][0] = new Cell(1, Cell.Diamond.DEFAULT, false);
+//                cells[0][1] = new Cell(2, Cell.Diamond.RED, false);
+//                cells[0][1] = new Cell(3, Cell.Diamond.GREEN, false);
+//                break;
+//        }
         switch(level){
             case 1:
                 cells = new Cell[2][2];
-                cells[0][0] = new Cell(1, Cell.Diamond.DEFAULT, false);
-                cells[0][1] = new Cell(2, Cell.Diamond.RED, false);
-                cells[1][0] = new Cell(3, Cell.Diamond.GREEN, false);
-                cells[1][1] = new Cell(4, Cell.Diamond.GREEN, false);
+                cells[0][0] = new Cell(1, Cell.Diamond.DEFAULT, true, 0, 0);
+                cells[0][1] = new Cell(2, Cell.Diamond.RED, false, 0, 1);
+                cells[1][0] = new Cell(3, Cell.Diamond.GREEN, false, 1, 0);
+                cells[1][1] = new Cell(4, Cell.Diamond.GREEN, false, 1, 1);
                 break;
-            case 2:
-                cells = new Cell[3][3];
-                cells[0][0] = new Cell(1, Cell.Diamond.DEFAULT, false);
-                cells[0][1] = new Cell(2, Cell.Diamond.RED, false);
-                cells[0][1] = new Cell(3, Cell.Diamond.GREEN, false);
-                break;
+//            case 2:
+//                cells = new Cell[3][3];
+//                cells[0][0] = new Cell(1, Cell.Diamond.DEFAULT, false);
+//                cells[0][1] = new Cell(2, Cell.Diamond.RED, false);
+//                cells[0][1] = new Cell(3, Cell.Diamond.GREEN, false);
+//                break;
         }
         return cells;
     }
