12f509bfc73d754e11c2cd301eddcf843a66c4f6
Rotih <rohitbathula2020@gmail.com>
2023-07-24 14:55:47 -0700

    removed redundancy in world

--- World.java
+++ World.java
@@ -29,68 +29,7 @@ public class World extends JFrame {
         }
         setSize(width,height);
         setBounds(0, 0, width, height);
-        DrawGrid draw_grid = new DrawGrid();
-        add(draw_grid);
         pack();
     }
 
-
-
-    public void run(){
-        ArrayList<Block> currBlocks = DataSource.getInstance().getConnectedBlocks();
-        for (Block block: currBlocks){
-            if (block.getType().equals("turn")){
-                spider.turn();
-            }
-            else if (block.getType().equals("step")){
-                int newRow = spider.row;
-                int newCol = spider.col;
-                if (spider.dir == 0){
-                    newRow--;
-                }
-                else if (spider.dir == 1){
-                    newCol++;
-                }
-                else if (spider.dir == 2){
-                    newRow++;
-                }
-                else{
-                    newCol--;
-                }
-                if (newRow <0 || newCol < 00 || newRow >= arr.length || newCol >= arr[0].length){
-                    // spider out of bounds
-                    //possible pop up box
-                    break;
-                }
-                else{
-                    arr[spider.row][spider.col].spider = null;
-                    spider.move();
-                    arr[spider.row][spider.col].spider = spider;
-                }
-            }
-            else if (block.getType().startsWith("paint")){
-                String [] words = block.getType().split("\\s+");
-                arr[spider.row][spider.col].setColor(words[1]);
-            }
-            repaint();
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-    public class DrawGrid extends JPanel
-    {
-        public void paint(Graphics g){
-
-            for (int row = 0; row < arr.length; row++){
-                for (int col = 0; col < arr[row].length; col++){
-                    int tempId = arr[row][col].id;
-                    arr[row][col].draw(g, tempId, row, col);
-
-                }
-            }
-        }
-    }
 }
